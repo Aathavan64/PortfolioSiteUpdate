1bb426b9ba47a2021c49baee4655b114348ba4c5
Viji <aathavan.bagawan@gmail.com>
2018-03-12 20:57:11 -0400

    updated styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    $('a').smoothScroll();
+});
+
+
 function backGroundToggle(x) {
     $(`.project${x}Link`).hover(function () {
         $(`.project${x}`).toggleClass('backGroundOff backGroundOn');
@@ -8,6 +13,7 @@ function backGroundToggle(x) {
 backGroundToggle('One');
 backGroundToggle('Two');
 backGroundToggle('Three');
+backGroundToggle('Four');
 
 
 // function backGroundFade(x) {
@@ -27,23 +33,22 @@ backGroundToggle('Three');
 
 
 
-function debounce(wait) {
-    return function(x){
-        $(`.project${x}Link`)
-            .mouseenter(function () {
-                $(`.project${x}`).animate({ opacity: 0 }, 0).css('background-image',
-                    `url(../images/project${x}Background.jpg)`).animate({ opacity: 1 }, 2500);
-                console.log('fadein')
-            })
-            .mouseleave(function () {
-                $(`.project${x}`).animate({ opacity: 1 }, 0).css('background-image',
-                    `url(../images/project${x}Background.jpg)`).animate({ opacity: 0 }, 2500);
-                console.log('fadeout')
-            });  
-    };
-};
+// function debounce(wait) {
+//     return function(x){
+//         $(`.project${x}Link`)
+//             .mouseenter(function () {
+//                 $(`.project${x}`).animate({ opacity: 0 }, 0).css('background-image',
+//                     `url(../images/project${x}Background.jpg)`).animate({ opacity: 1 }, 2500);
+//                 console.log('fadein')
+//             })
+//             .mouseleave(function () {
+//                 $(`.project${x}`).animate({ opacity: 1 }, 0).css('background-image',
+//                     `url(../images/project${x}Background.jpg)`).animate({ opacity: 0 }, 2500);
+//                 console.log('fadeout')
+//             });  
+//     };
+// };
 
-$('.landing').parallax({ imageSrc: '../images/landingBackground.png' });
 
 
 
